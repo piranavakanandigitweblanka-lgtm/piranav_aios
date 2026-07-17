fdd2746e291a5740b808a5eaf64141509d3fd5b9
Piranav <adsdm03@gmail.com>
2026-07-17 10:43:53 +0530

    fix: disable SSL for PostgreSQL (server does not support SSL)

--- Staff-requirements-02/api/sonya/campaign-performance.js
+++ Staff-requirements-02/api/sonya/campaign-performance.js
@@ -16,7 +16,7 @@ module.exports = async function handler(req, res) {
 
   const client = new Client({
     connectionString: connStr,
-    ssl: { rejectUnauthorized: false },
+    ssl: false,
     connectionTimeoutMillis: 15000,
     statement_timeout: 12000,
   });
